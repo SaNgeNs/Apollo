ad6c1b1f5bd9f339e67608d5f68765fe81e5f6e3
lven <latyshevevgen91@gmail.com>
2020-06-20 02:37:33 +0300

    fix ff gzip

--- src/server.jsx
+++ src/server.jsx
@@ -15,14 +15,16 @@ const statsFile = path.resolve('./build/spa/loadable-stats.json');
 
 const app = express();
 
+app.use(compression());
+
 app.get('*.js', (req, res, next) => {
   req.url = req.url + '.gz';
   res.set('Content-Encoding', 'gzip');
+  res.set('Content-Type', 'text/plain');
   next();
 });
 
 app.use(express.static('build/spa'));
-app.use(compression());
 
 app.get('*', (req, res, next) => {
   const extractor = new ChunkExtractor({
